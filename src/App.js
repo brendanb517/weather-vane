3cdfcea822154155cfedd79fb5085873659895b1
Brendan <brendan.b517@gmail.com>
2023-08-27 18:07:47 -0400

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=newyork&appid=a2de3f31b6c331bd2ecedc02045e0240`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Atlanta</p>
+          </div>
+          <div className='temp'>
+            <h1>96°F</h1>
+          </div>
+          <div className='description'>
+            <p>Thunderstorm</p>
+          </div>
+        </div>
+        <div className='bottom'>
+          
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
